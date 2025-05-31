88129d9010bde481deb368ce32d47cf910e5989e
BASYME <dan.eremok@gmail.com>
2025-05-31 10:02:51 +0300

    Добавление поля с редактированием удалением и копированием пароля, изменено окно добавление новой записи, добавлен генератор паролей, добавлена функция скрыть/показать пароль

--- pass_open.py
+++ pass_open.py
@@ -1,10 +1,15 @@
+__version__ = "0.2.0"
+
+
 import sys
 import json
 import os
 from PySide6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QPushButton, QListWidget,
-    QInputDialog, QLineEdit, QMessageBox
+    QInputDialog, QLineEdit, QMessageBox, QListWidgetItem, QHBoxLayout, QLabel, QMenu,
+    QDialog, QDialogButtonBox, QVBoxLayout, QLineEdit, QFormLayout
 )
+from PySide6.QtCore import Qt
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet, InvalidToken
@@ -44,6 +49,77 @@ def load_vault(key):
         return json.loads(f.decrypt(enc).decode())
     except (FileNotFoundError, InvalidToken):
         return {"accounts": []}
+    
+
+
+# ----- Диалог добавления/редактирования записи -----
+class AddEditDialog(QDialog):
+        def __init__(self, name='', username='', password=''):
+            super().__init__()
+            self.setWindowTitle("Добавить запись")
+            self.setMinimumWidth(300)
+
+            layout = QVBoxLayout()
+
+            self.name_edit = QLineEdit(name)
+            self.username_edit = QLineEdit(username)
+            
+            # --- Пароль ---
+            pass_layout = QHBoxLayout()
+            self.password_edit = QLineEdit(password)
+            self.password_edit.setEchoMode(QLineEdit.Password)
+            pass_layout.addWidget(self.password_edit)
+            self.toggle_btn = QPushButton("👁")
+            self.toggle_btn.setCheckable(True)
+            self.toggle_btn.setFixedWidth(32)
+            self.toggle_btn.clicked.connect(self.toggle_password)
+            pass_layout.addWidget(self.toggle_btn)
+            # --- Поля ввода ---
+            layout.addWidget(QLabel("Название сервиса:"))
+            layout.addWidget(self.name_edit)
+            layout.addWidget(QLabel("Имя пользователя:"))
+            layout.addWidget(self.username_edit)
+            layout.addWidget(QLabel("Пароль:"))
+            layout.addLayout(pass_layout)
+
+            # --- Генерация пароля ---
+            btn_generate = QPushButton("Сгенерировать пароль")
+            btn_generate.clicked.connect(self.generate_password)
+            layout.addWidget(btn_generate)
+
+            buttons = QHBoxLayout()
+            btn_ok = QPushButton("OK")
+            btn_cancel = QPushButton("Отмена")
+            btn_ok.clicked.connect(self.accept)
+            btn_cancel.clicked.connect(self.reject)
+            buttons.addWidget(btn_ok)
+            buttons.addWidget(btn_cancel)
+            layout.addLayout(buttons)
+
+            self.setLayout(layout)
+
+        def generate_password(self):
+                import secrets
+                import string
+                chars = string.ascii_letters + string.digits + string.punctuation
+                pdw = ''.join(secrets.choice(chars) for _ in range(16))
+                self.password_edit.setText(pdw)
+
+        def get_data(self):
+                return {
+                    "name": self.name_edit.text(),
+                    "username": self.username_edit.text(),
+                    "password": self.password_edit.text()
+                }
+        def toggle_password(self):
+            if self.toggle_btn.isChecked():
+                self.password_edit.setEchoMode(QLineEdit.Normal)
+                self.toggle_btn.setText("🙈")
+            else:
+                self.password_edit.setEchoMode(QLineEdit.Password)
+                self.toggle_btn.setText("👁")
+
+
 
 # ----- Главное окно -----
 class MainWindow(QWidget):
@@ -65,22 +141,102 @@ class MainWindow(QWidget):
 
         self.refresh_list()
 
+    def add_entry(self):
+        dialog = AddEditDialog()
+        if dialog.exec() == QDialog.Accepted:
+            name, username, password = dialog.get_data()
+            if not name or not username or not password:
+                QMessageBox.warning(self, "Ошибка", "Все поля должны быть заполнены.")
+                return
+            self.vault["accounts"].append({"name": name, "username": username, "password": password})
+            save_vault(self.vault, self.key)
+            self.refresh_list()
+
+                
+
+
     def refresh_list(self):
         self.list_widget.clear()
-        for acc in self.vault["accounts"]:
-            self.list_widget.addItem(f"{acc['name']} ({acc['username']})")
+        for idx, acc in enumerate(self.vault["accounts"]):
+            widget = AccountWidget(acc, self, idx)
+            item = QListWidgetItem(self.list_widget)
+            item.setSizeHint(widget.sizeHint())
+            self.list_widget.addItem(item)
+            self.list_widget.setItemWidget(item, widget)
+        
 
-    def add_entry(self):
-        name, ok1 = QInputDialog.getText(self, "Название сервиса", "Введите название:")
-        if not ok1 or not name: return
-        username, ok2 = QInputDialog.getText(self, "Имя пользователя", "Введите логин/имя:")
-        if not ok2 or not username: return
-        password, ok3 = QInputDialog.getText(self, "Пароль", "Введите пароль:", QLineEdit.Password)
-        if not ok3 or not password: return
 
-        self.vault["accounts"].append({"name": name, "username": username, "password": password})
-        save_vault(self.vault, self.key)
-        self.refresh_list()
+    # ----- Копирование пароля в буфер обмена -----
+    def copy_password(self, index):
+        password = self.vault["accounts"][index]["password"]
+        QApplication.clipboard().setText(password)
+        QMessageBox.information(self, "PassOpen", "Пароль скопирован в буфер обмена.")
+
+    def delete_entry(self, index):
+        reply = QMessageBox.question(self, "Удаление", "Вы уверены, что хотите удалить эту запись?", QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            del self.vault["accounts"][index]
+            save_vault(self.vault, self.key)
+            self.refresh_list()
+
+    def edit_entry(self, index):
+        acc = self.vault["accounts"][index]
+        dialog = AddEditDialog(acc["name"], acc["username"], acc["password"])
+        if dialog.exec() == QDialog.Accepted:
+            data = dialog.get_data()
+            self.vault["accounts"][index] = {
+                "name": data["name"],
+                "username": data["username"],
+                "password": data["password"]
+            }
+            save_vault(self.vault, self.key)
+            self.refresh_list()
+
+
+
+
+
+
+# ----- Виджет для отображения и редактирования записи -----
+
+class AccountWidget(QWidget):
+    def __init__(self, account, parent, index):
+        super().__init__()
+        self.account = account
+        self.index = index
+        self.parent = parent
+
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(5, 2, 5, 2)
+        layout.setAlignment(Qt.AlignVCenter)
+
+
+        label = QLabel(f"{account['name']} ({account['username']})")
+        layout.addWidget(label)
+
+        btn = QPushButton("⋮")
+        btn.setFixedWidth(40)
+        btn.setFlat(True)
+        btn.setStyleSheet("font-size: 18px;")
+        layout.addWidget(btn, alignment=Qt.AlignRight)
+
+        menu = QMenu(self)
+        action_copy = menu.addAction("Копировать пароль")
+        action_edit = menu.addAction("Редактировать")
+        action_delete = menu.addAction("Удалить")
+
+        def show_menu():
+            menu.exec(btn.mapToGlobal(btn.rect().bottomRight()))
+        btn.clicked.connect(show_menu)
+
+        action_copy.triggered.connect(lambda: self.parent.copy_password(self.index))
+        action_edit.triggered.connect(lambda: self.parent.edit_entry(self.index))
+        action_delete.triggered.connect(lambda: self.parent.delete_entry(self.index))
+        
+        
+
+
+
 
 # ----- Стартовая точка -----
 def get_or_create_salt():
